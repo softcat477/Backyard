b9c7cb7a7fb176b00b3d2a57beabccbea3c3ac1e
softcat477 <softcat477@gmail.com>
2023-06-18 15:52:45 -0400

    Fix #2, Add trajectory prediction module
    - Add scripts/assset/materials to predict the trajectory of a projectile
    - Disable player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,130 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public enum FireStatus
+{
+    None, Aim, Fire
+}
+
+/// <summary>
+/// This class controls the player's input and manages the player's interaction with the game world,
+/// including aiming and firing projectiles using a drag mechanism.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    private PlayerInputActions playerInputActions;
+    private InputAction leftClick;
+    private InputAction mouseMove;
+
+    // The click position (screen space) when the user enters the Aim state and moves to the Fire state in the next update
+    private Vector2 leftClickOrigin;
+
+    // Instantiate a new <ballPrefab> GameObject at position + prefabOffset (world space) and let <launchedBullet> keep it
+    public GameObject ballPrefab;
+    private GameObject launchedBullet;
+    public Vector3 prefabOffset = new Vector3(-3.25f, 3.49f, 4.26f);
+
+    [SerializeField] private TrajectorySimulator trajectorySimulator;
+    private FireStatus fireStatus;
+    // Apply force to <launchedBullet> and use the force to simulate its trajectory
+    private Vector3 force;
+
+    // Drag GUI to visualize drag on screen
+    //public GameObject dragVisualizer;
+
+    private void Awake() {
+        playerInputActions = new PlayerInputActions();
+        fireStatus = FireStatus.None;
+        launchedBullet = null;
+        //dragVisualizer.SetActive(false);
+
+        if (!ballPrefab.CompareTag("Projectile")) {
+            Debug.LogError("TrajectorySimulator.cs requires ballPrefab to be tagged as Projectile. Get tag: " + ballPrefab.tag);
+        }
+    }
+
+    private void OnEnable() {
+        leftClick = playerInputActions.Player.LeftClick;
+        leftClick.Enable();
+        leftClick.performed += OnLeftClick;
+        leftClick.canceled += OnLeftRelease;
+
+        mouseMove = playerInputActions.Player.MouseMove;
+        mouseMove.Enable();
+    }
+
+    private void OnDisable() {
+        leftClick.Disable();
+        mouseMove.Disable();
+    }
+
+    private void Update() {
+        if (fireStatus == FireStatus.Aim) {
+            Vector2 releasePosition = mouseMove.ReadValue<Vector2>();
+            Vector2 diff = releasePosition - leftClickOrigin;
+
+            // Visualize the mouse movement
+            //dragVisualizer.GetComponent<DragVisualizer>().SetTrace(releasePosition);
+
+            // Use x's offset as the rotated degree of the forward direction
+            // Map mouse's x offset from -200px ~ +200px to -90 degrees ~ +90 degrees
+            float degree = Mathf.Clamp(diff.x, -200.0f, 200.0f) * 90 / 200;
+
+            // Use y's offset as the magnitude
+            // Map mouse's y offset from -20 px (drag down) ~ 0px to 10 ~ 0
+            float magnitude = Mathf.Clamp(-diff.y, 0, Mathf.Sqrt(200*200*2));
+            magnitude = magnitude / Mathf.Sqrt(200*200*2) * 10;
+
+            // Instantiate and hold a new bullet
+            if (launchedBullet == null) {
+                launchedBullet = Instantiate(ballPrefab, transform.TransformPoint(prefabOffset), transform.rotation);
+                launchedBullet.transform.SetParent(transform);
+                launchedBullet.GetComponent<Rigidbody>().isKinematic = true; // to stop physics simulation or else the projectile will fall
+            }
+
+            // Calculate the applied force. The bullet is always fired at an angle of 45 degree with the horizontal
+            force = Quaternion.AngleAxis(-45, launchedBullet.transform.right) * launchedBullet.transform.forward * magnitude;
+            // Rotate w.r.t the up vector of the bullet
+            force = Quaternion.AngleAxis(degree, launchedBullet.transform.up) * force;
+
+            // Simulate the trajectory using the applied force
+            OnSimulate(force, launchedBullet.GetComponent<Rigidbody>().mass, launchedBullet.transform);
+        }
+        else if (fireStatus == FireStatus.Fire) {
+            launchedBullet.transform.SetParent(transform.parent);
+            Rigidbody rb = launchedBullet.GetComponent<Rigidbody>();
+            rb.isKinematic = false;
+            rb.AddForce(force, ForceMode.Impulse);
+            fireStatus = FireStatus.None;
+            launchedBullet = null;
+        }
+    }
+
+    /// <summary>
+    /// The callback function when the user clicks the left mouse button. 
+    /// Save the click position to calculate the force applied to the bullet.
+    /// Activate the Drag GUI to visualize the mouse movement.
+    /// Move the GUI to be centered at the click position.
+    /// </summary>
+    private void OnLeftClick(InputAction.CallbackContext ctx) {
+        leftClickOrigin = mouseMove.ReadValue<Vector2>();
+        fireStatus = FireStatus.Aim;
+
+        //dragVisualizer.GetComponent<DragVisualizer>().SetAnchor(leftClickOrigin);
+        //dragVisualizer.SetActive(true);
+    }
+
+    /// <summary>
+    /// The callback function when the user releases the left mouse button.
+    /// </summary>
+    private void OnLeftRelease(InputAction.CallbackContext ctx) {
+        fireStatus = FireStatus.Fire;
+        //dragVisualizer.SetActive(false);
+    }
+
+    private void OnSimulate(Vector3 force, float mass, Transform prefabTransform) {
+        trajectorySimulator.StartSimulate(force, mass, prefabOffset, prefabTransform);
+    }
+}
---
+++ Assets/Scripts/TrajectorySimulator.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+/// <summary>
+/// Simulates the trajectory of a projectile based on a given force and mass,
+/// and visualizes the trajectory using a LineRenderer component. DDetects collisions 
+/// with colliders and displays an indicator at the collision point.
+/// </summary>
+[RequireComponent(typeof(LineRenderer))]
+public class TrajectorySimulator : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+
+    // The maximum number of simulated points
+    public int nPoint = 100;
+    public float timeStep = 1.0f/24.0f;
+
+    public GameObject indicatorPrefab;
+    GameObject indicator;
+
+    private List<Vector3> simPoints;
+    // Start is called before the first frame update
+    void Start()
+    {
+        lineRenderer = GetComponent<LineRenderer>();
+        simPoints = new List<Vector3>();
+        indicator = Instantiate(indicatorPrefab, transform.position, transform.rotation);
+        indicator.SetActive(false);
+    }
+
+    private float VecSum(Vector3 vec) {
+        return vec.x + vec.y + vec.z;
+    }
+
+
+    /// <summary>
+    /// Simulation of the projectile trajectory.
+    /// </summary>
+    /// <param name="force">The force applied to the projectile.</param>
+    /// <param name="mass">The mass of the projectile.</param>
+    /// <param name="offset">The offset of the projectile's initial position w.r.t the player positions.</param>
+    /// <param name="prefabTransform">The transform of the instantiated projectile.</param>
+    public void StartSimulate(Vector3 force, float mass, Vector3 offset, Transform prefabTransform) {
+        float time = 0.0f;
+        simPoints.Clear();
+        if (!indicator.activeSelf) {
+            indicator.SetActive(true);
+        }
+ 
+        Vector3 initialVelocity = (force / mass);
+
+        for (int i = 0; i < nPoint; i++) {
+            float dx = time * initialVelocity.x;
+            float dy = time * initialVelocity.y - 0.5f * 9.8f * time * time;
+            float dz = time * initialVelocity.z;
+            Vector3 tmp = new Vector3(dx, dy, dz);// + offset; // In world space
+            Vector3 displacement = tmp + transform.TransformPoint(offset);
+            simPoints.Add(displacement);
+
+            // Stop simulation when hitting a collider
+            Collider[] colliders = Physics.OverlapSphere(displacement, 0.25f);
+            colliders = colliders.Where(c => c.gameObject.CompareTag("Projectile") == false).ToArray();
+            if (colliders.Length > 0) {
+                Collider collider = colliders[0];
+
+                Vector3 collisionPoint = collider.ClosestPoint(displacement);
+                Quaternion rotation = Quaternion.FromToRotation(indicatorPrefab.transform.up, collider.transform.up);
+                indicator.transform.position = collisionPoint;
+                indicator.transform.rotation = rotation;
+
+                // Place the indicator to visualize the predicted position
+                // Check the project collides with inner or outer side
+                Vector3 toCollisionPoint = collisionPoint - transform.TransformPoint(offset);
+                bool collideFromOuter = Vector3.Dot(collider.transform.up, toCollisionPoint) < 0;
+                if (collideFromOuter) {
+                    indicator.transform.Translate(Vector3.up * 0.1f);
+                }
+                else {
+                    indicator.transform.Translate(Vector3.up * -0.1f);
+                }
+                break;
+            }
+            time += timeStep;
+        }
+
+        lineRenderer.positionCount = simPoints.ToArray().Length;
+        lineRenderer.SetPositions(simPoints.ToArray());
+    }
+}
